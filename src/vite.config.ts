275115bde81319bb9468ae1c6f374fa574a2501b
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-03-26 07:29:58 +0900

    did it

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const isLadle = process.argv[1]?.includes("ladle");
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(),!isLadle && reactRouter(), tsconfigPaths()],
 });
